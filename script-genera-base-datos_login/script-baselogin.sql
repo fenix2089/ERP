80c8ac5dca149560a49a787579aac85b020081bd
Carlos Soto <39345128+fenix2089@users.noreply.github.com>
2019-09-09 11:31:46 -0600

    Update script-baselogin.sql

--- script-genera-base-datos_login/script-baselogin.sql
+++ script-genera-base-datos_login/script-baselogin.sql
@@ -9,8 +9,4 @@ CREATE TABLE  usuarios(
 ) ENGINE=INNODB;
 
 INSERT INTO `usuarios` (`Id`, `nombre`, `clave`, `tipo`) VALUES
-(1, 'Yasmin', '$2y$10$.NkSEXbrMaYcE8bf9VnR9uD2KtfqUqSeHQUyuDfxDHClQjILr3EsS', 'admin'),
-(2, 'Carlos', '$2y$10$S0OXU.5avDXL1mbN0/oO6O7/lqd5NiwEqHJ6lbtcOc.eHm/y8NsDW', 'admin'),
-(3, 'Arturo', '$2y$10$zuXX1HpvMJccZYGgXatVzOpQ5tkmBIUhHRC253I8LcCDagasLAEuq', 'admin');
-
--- creen 1 usuario mas cada uno pero lo hacen tipo operador y inician sesión con ese nuevo usuario.
+(1, 'Carlos', '$2y$10$S0OXU.5avDXL1mbN0/oO6O7/lqd5NiwEqHJ6lbtcOc.eHm/y8NsDW', 'admin');
