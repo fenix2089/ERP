b13e5df9e0d7ce19eac221c4181c01aa7cc0d8db
Carlos Soto <39345128+fenix2089@users.noreply.github.com>
2019-09-09 11:37:11 -0600

    Update script-baselogin.sql

--- script-genera-base-datos_login/script-baselogin.sql
+++ script-genera-base-datos_login/script-baselogin.sql
@@ -3,10 +3,10 @@ USE baselogin;
 
 CREATE TABLE  usuarios(
 	Id INT (255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    nombre varchar(255),
-    clave varchar(255),
-    tipo varchar(50)
-) ENGINE=INNODB;
+	nombre varchar(255),
+	clave varchar(255),
+	tipo varchar(50)
+)ENGINE=INNODB;
 
 INSERT INTO `usuarios` (`Id`, `nombre`, `clave`, `tipo`) VALUES
 (1, 'Carlos', '$2y$10$S0OXU.5avDXL1mbN0/oO6O7/lqd5NiwEqHJ6lbtcOc.eHm/y8NsDW', 'admin');
